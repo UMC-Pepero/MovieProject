7ae12b57eb3c444ebfee8b3033dd7cdb9bf4ecce
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-05-02 18:00:50 +0900

    comment 구현 완료

--- DetailPage/comment.js
+++ DetailPage/comment.js
@@ -1,87 +1,83 @@
-const idPasswordForm = document.querySelector(".id-password__box");
+const commentForm = document.querySelector(".comment-write__box");
 const usernameInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
-const commentWriteForm = document.querySelector(".comment-write");
 const commentInput = document.getElementById("write");
 
-//1. idPasswordForm이 submit되면 localStorage에 id, password 값 저장
-//2. 저장되면 idPasswordForm hidden className추가해서 가려주고,
-//3. idPasswordForm submit 해서 localStorage에 값이 있어야지 commentWriteForm submit 가능
-//3. 작성한 정보 localStorage에 적재.
-
-const submitIdForm = (event) => {
+const handleSubmitForm = (event) => {
   event.preventDefault();
-  idPasswordForm.classList.add("hidden");
+  // 입력된 이름, 댓글 내용 가져오기
   const username = usernameInput.value;
   const password = passwordInput.value;
-  localStorage.setItem("Username", username);
-  localStorage.setItem("Password", password);
-  commentWriteForm.classList.remove("hidden");
+  const comment = commentInput.value;
+  //값 비우기, 변수의 값에는 영향 x
+  usernameInput.value = "";
+  passwordInput.value = "";
+  commentInput.value = "";
+  //로컬 스토리지에 username이랑 password 저장
+
+  // 값을 담을 새로운 객체 생성
+  let newComment = {
+    user: username,
+    password: password,
+    review: comment,
+  };
+  // 1. 로컬스토리지에서 가져온다.
+  const commentsArr = getComments();
+
+  // 새로운 배열에 객체 값 넣어주기
+  // 2. 로컬스토리지에 있는 데이터에 추가한다.
+  commentsArr.unshift(newComment); //push는 배열 뒤부터 추가, unshift는 앞에서부터 추가
+
+  saveComments(commentsArr); //commentsArr배열을 추가해서 저장
+  generateComment(newComment);
 };
 
-idPasswordForm.addEventListener("submit", submitIdForm);
+commentForm.addEventListener("submit", handleSubmitForm);
 
-let comments = [];
-
-// commentInput.addEventListener("focus", () => alert("로그인을 해주세요."));
-
-const submitCommentForm = (event) => {
-  event.preventDefault();
-  let comment = commentInput.value;
-  comments.push(comment);
-  localStorage.setItem("Comment", comment);
-  //   if (username === null) {
-  //     alert("로그인을 해주세요");
-  //   } else {
-  //     comment = "";
-  //     generateComment();
-  //   }
+//여기서 comments는 인자로 사용
+const saveComments = (comments) => {
+  localStorage.setItem("comments", JSON.stringify(comments));
 };
 
-console.log({ comments }); //로컬스토리지에 저장된 배열 확인.
+// 로컬 스토리지에서 댓글 배열 가져오기
+const getComments = () => {
+  const savedComments = localStorage.getItem("comments");
 
-// //Enter 키 입력해도 값 처리하기
-// const enterKey = () => {
-//   if (window.event.keyCode === 13) {
-//     submitCommentForm.submit();
-//   }
-// };
+  if (savedComments) {
+    return JSON.parse(savedComments); //다시 배열로 바꿔주기
+  } else {
+    return [];
+  }
+};
 
-// enterKey();
-
-commentWriteForm.addEventListener("submit", submitCommentForm);
-
-const savedUsername = localStorage.getItem("Username");
-const savedComment = localStorage.getItem("Comment");
-//section comment 부분에 변수로 아이템 넣어주기.
-
-const generateComment = (event) => {
+const generateComment = (newComment) => {
   const commentBox = document.querySelector(".comment__wrapper");
+  commentBox.innerHTML = "";
 
-  commentBox.innerHTML += `
+  let commentDrawn = getComments();
+  //commentsArr를 순회하면서 댓글 그리기
+  commentDrawn.forEach((element) => {
+    commentBox.innerHTML += `
   <li class="comment__box">
     <img
     class="user-image"
     src="https://static.vecteezy.com/system/resources/thumbnails/005/276/776/small/logo-icon-person-on-white-background-free-vector.jpg"
     />
     <section class="comment">
-    <h4>${savedUsername}</h4>
-    <p>
-        ${savedComment}
-    </p>
+    <h4>${element.user}</h4>
+    <p>${element.review}</p>
     </section>
     <div class="rate">
     <i class="fa-regular fa-thumbs-up fa-lg"></i>
     <span>55</span>
     </div>
 </li>`;
+  });
 };
 
-// 3.로컬스토리지에서 댓글 배열 가져오기
-
 // 4. 페이지가 로드될 때 기존 댓글 불러오기
-// window.onload = function () {
-//     loadComments();
-//   };
+window.onload = function () {
+  generateComment();
+};
 
 //5. 댓글 목록 불러오기
