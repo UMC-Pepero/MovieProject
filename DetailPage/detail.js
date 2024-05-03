e49a9f2e6156744ae1d5f48e364c1d9413475585
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-05-03 13:03:16 +0900

    댓글창 수정 삭제 버튼 추가

--- DetailPage/detail.js
+++ DetailPage/detail.js
@@ -176,11 +176,15 @@ const generateComment = (comments) => {
         />
         <section class="comment">
           <div class="userInfo">
-            <h4>${element.user}</h4>
-            <span class="starsIcon material-symbols-outlined" style="font-size: 18px"">kid_star</span>
-            <span class="stars">${element.rating}</span>
+              <h4>${element.user}</h4>
+              <span class="starsIcon material-symbols-outlined" style="font-size: 18px"">kid_star</span>
+              <span class="stars">${element.rating}</span>
           </div>
           <p>${element.review}</p>
+          <div class="edit-delete" >
+            <button id="edit"><i class="fa-solid fa-pen fa-lg"></i></button>
+            <button id="delete"><i class="fa-regular fa-trash-can fa-lg"></i></i></button>
+          </div>
         </section>
         <div class="rate">
           <i class="fa-regular fa-thumbs-up fa-lg"></i>
@@ -224,3 +228,8 @@ const highlightStars = (value) => {
     }
   });
 };
+
+//댓글 삭제하기
+//댓글 수정하기
+//로컬스토리지 아이템 갯수 -> 총 댓글 수에 반영하기
+const commentLength = window.localStorage.length;
