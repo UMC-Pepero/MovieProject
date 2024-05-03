f4dae666b204479a89a2f79a5980bb1790b55e10
youngsupark1 <eddy364885@gmail.com>
2024-05-03 16:19:18 +0900

    dd

--- DetailPage/detail.js
+++ DetailPage/detail.js
@@ -238,21 +238,6 @@ const highlightStars = (value) => {
   });
 };
 
-// (수정)따봉 버튼을 누르면 숫자가 올라가게끔
-function count(type) {
-  // 결과를 표시할 element
-  const resultElement = document.getElementById("result");
-
-  // 현재 화면에 표시된 값
-  let number = resultElement.innerText;
-
-  // 더하기/빼기
-  if (type === "plus") {
-    number = parseInt(number) + 1;
-  }
-  // 결과 출력
-  resultElement.innerText = number;
-}
 //댓글 삭제하기
 //댓글 수정하기
 
